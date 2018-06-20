fe597d54bb27dec6ef46060ccffb2b0832552583
Ivan Baydanutsa <zulin@in-tag.com>
2018-06-20 11:36:29 +0300

    added blur fix for reset command

--- src/views/View.js
+++ src/views/View.js
@@ -131,6 +131,7 @@ export default class View {
   _setPage(page: number) {
     this.pageNumber = page;
     this._updateScene();
+    this.scenes[i]['scene'].scene.parent.filters = [];
   }
 
   _updateScene() {
